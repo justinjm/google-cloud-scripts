de187653e314678b084dc17a83ba302a90ef0bc2
Justin Marciszewski <justinjm@google.com>
2023-01-31 09:38:37 -0500

    simplifying

--- cloud_storage.py
+++ cloud_storage.py
@@ -1,16 +1,11 @@
+from google.cloud import storage
+from google.cloud.exceptions import NotFound
 
-# check if bucket exists
-def check_bucket_existence(PROJECT_ID, BUCKET_PATH):
-    from google.cloud import storage
-    from google.cloud.storage import Bucket
-
-    client = storage.Client(project=PROJECT_ID)
-    exists = Bucket(client, BUCKET_PATH).exists()
-    return exists
-
-# create bucket if needed
-if check_bucket_existence(PROJECT_ID, BUCKET_NAME) == False:
-    ! gsutil mb -c standard -l $REGION -b on -p $PROJECT_ID $BUCKET_PATH
-    ! gsutil ls
-else:
-    print(f"Bucket {BUCKET_NAME} already exists")
+def check_and_create_bucket(bucket_name):
+    client = storage.Client()
+    try:
+        client.get_bucket(bucket_name)
+        print(f"Bucket {bucket_name} already exists.")
+    except NotFound:
+        bucket = client.create_bucket(bucket_name)
+        print(f"Bucket {bucket_name} created.")
