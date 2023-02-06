6bc446c18ba005576cc34eca3ca88bd712a7492b
Justin Marciszewski <justinjm@google.com>
2023-02-06 16:15:01 -0500

    successful test of gcs and bq

--- R/multi-auth.R
+++ R/multi-auth.R
@@ -1,4 +1,4 @@
-
+# multi-auth.R ---------------------------------------------------------------
 ## set constants 
 ### defaults from .Renviron 
 project_id <- Sys.getenv("GCP_PROJECT_ID")
@@ -9,7 +9,7 @@ dataset <- Sys.getenv("BQ_DEFAULT_DATASET")
 
 ## load packages
 library(googleCloudStorageR)
-# library(bigrquery)
+library(bigrquery)
 library(gargle)
 
 ## authenticate 
@@ -18,27 +18,29 @@ scope <- c("https://www.googleapis.com/auth/cloud-platform")
 token <- token_fetch(scopes = scope,
                      email = email)
 gcs_auth(token = token)
+
 ### BQ 
-# bq_auth(email = email)
+bq_auth(email = email)
 
 ## check authentication
 gcs_list_buckets(projectId = project_id)
-# bq_project_datasets(project_id)
+bq_project_datasets(project_id)
 
-# gargle 
+
+## References 
+### gargle 
 # https://cran.r-project.org/web/packages/gargle/vignettes/gargle-auth-in-client-package.html#getting-that-first-token
 
-# gcs 
+## gcs 
 # https://code.markedmondson.me/googleCloudStorageR/reference/gcs_auth.html
 
-# bq 
+## bq 
 # [bq\_auth()](https://github.com/r-dbi/bigrquery/blob/main/R/bq-auth.R#L59) calls 
 # [gargle::token\_fetch](https://github.com/r-lib/gargle/blob/main/R/token-fetch.R) function.
 # This function uses multiple methods to fetch the auth token.
 # 
 # On GCE instances [credentials\_gce]() function will be used.
 # 
-# bq\_auth function [uses the following scope]() by default:
-# 
-# 
-# https://github.com/r-lib/gargle/issues/49
+
+### other  
+# multi auth question - https://github.com/r-lib/gargle/issues/49
