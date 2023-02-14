9cbf1c067c2155d6064917c6fdbafb59e0cd9a30
Justin Marciszewski <justinjm@google.com>
2023-02-14 12:14:08 -0500

    re-testing with schema

--- bq_migration/loanstest.sql
+++ bq_migration/loanstest.sql
@@ -4,22 +4,23 @@ go
 
 -- create table
 CREATE TABLE test.loans(
-   id                INTEGER  PRIMARY KEY 
-  ,member_id         INTEGER 
-  ,loan_amnt         INTEGER 
-  ,term_in_months    INTEGER 
+   id                INT  PRIMARY KEY 
+  ,member_id         INT 
+  ,loan_amnt         INT 
+  ,term_in_months    INT 
   ,interest_rate     NUMERIC(5,2)
   ,payment           NUMERIC(7,2)
   ,grade             VARCHAR(1)
   ,sub_grade         VARCHAR(2)
-  ,employment_length INTEGER 
-  ,home_owner        BIT 
+  ,employment_length INT 
+  ,home_owner        INT 
   ,income            NUMERIC(9,2)
-  ,verified          BIT 
-  ,`default`           BIT 
+  ,verified          INT 
+  ,`default`         INT
   ,purpose           VARCHAR(18)
   ,zip_code          VARCHAR(5)
   ,addr_state        VARCHAR(2)
-  ,open_accts        INTEGER 
-  ,credit_debt       INTEGER 
+  ,open_accts        INT 
+  ,credit_debt       INT
 );
+
