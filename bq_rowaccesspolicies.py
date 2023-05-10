552a276d8d2d21404cf0418706b57a1d95800b09
Justin Marciszewski <justinjm@google.com>
2023-05-10 15:38:24 -0400

    adding comment

--- bq_rowaccesspolicies.py
+++ bq_rowaccesspolicies.py
@@ -26,6 +26,7 @@ response = requests.get(url, headers=headers)
 # Print the response
 print(json.dumps(response.json(), indent=4))
 
+# as of 2023-05-10 - not able to view row-level access policies within BQ UI programmatically 
 # https://cloud.google.com/bigquery/docs/managing-row-level-security#create_or_update_a_row-level_access_policy
 # https://cloud.google.com/bigquery/docs/reference/rest/v2/rowAccessPolicies/list
 # https://towardsdatascience.com/remote-functions-in-bigquery-af9921498438
